65bb7d976094b47eec0db366bba4dccbfb8f1e4b
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-08-29 22:17:23 -0400

    utils.py -> definition of repeter_mot(),somme_liste(),separe_text(); main.py -> test_utils()

--- Recap/main.py
+++ Recap/main.py
@@ -1,7 +1,23 @@
 import utils as ut
 
+# ------------------------------------------------------------------------------
+
+def test_utils():
+	ut.repeter_mot( "Bonjour", 0)
+	ut.repeter_mot( "Impossible", -1 )
+	ut.repeter_mot( "Répéter", 3 )
+	
+	numbers: list[int] = [-1, 2, 3, 4]
+	print( ut.somme_liste( numbers ) )
+
+	test_tuple = ut.separe_text( "LeftHandSide:RightHandSide:test", ":" )
+	print( test_tuple[0] )
+	print( test_tuple[1] )
+
+# --------------------------------------------------------------------- [ MAIN ]
+
 def main():
-	pass
+	test_utils()
 
 if __name__ == "__main__":
     main()
--- Recap/utils.py
+++ Recap/utils.py
@@ -1,14 +1,20 @@
 def repeter_mot(
 	mot: str,
 	n: int
-):
-	pass
+) -> None:
+	while n > 0:
+		print( mot )
+		n -= 1
+	return
 
-def somme_liste( num: list[int] ):
-	pass
+def somme_liste( numbers: list[int] ) -> int:
+	sum = 0
+	for number in numbers:
+		sum += number
+	return sum
 
 def separe_text(
 	texte: str,
 	sep: str
-):
-	pass
+) -> tuple[str,str]:
+	return texte.split( sep, 1 )
