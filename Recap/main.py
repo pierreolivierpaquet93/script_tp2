f62bf192acada3cc83a22c560918f17239b93700
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-08-29 21:54:22 -0400

    +main.py; +Instrument.py; +Media.py; +Porte.py; +utils.py

---
+++ Recap/main.py
@@ -0,0 +1,7 @@
+import utils as ut
+
+def main():
+	pass
+
+if __name__ == "__main__":
+    main()
---
+++ Recap/utils.py
@@ -0,0 +1,14 @@
+def repeter_mot(
+	mot: str,
+	n: int
+):
+	pass
+
+def somme_liste( num: list[int] ):
+	pass
+
+def separe_text(
+	texte: str,
+	sep: str
+):
+	pass
