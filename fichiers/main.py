094bdaad673730f08c5ff979f30f028fc0d26f59
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-08-30 23:00:14 -0400

    +fichier folder; +main(); +Prix.txt; +Recette.txt; +CONSTANTS

---
+++ fichiers/main.py
@@ -0,0 +1,22 @@
+import Fichiers as fi
+
+# -------------------------------------------------------------------- [ FILES ]
+
+CURRENT_DIR = "./"
+
+PRICE_FILENAME = "Prix.txt"
+PRICE_FILE = f"{CURRENT_DIR}{PRICE_FILENAME}"
+
+RECIPE_FILENAME = "Recette.txt"
+RECIPE_FILE = f"{CURRENT_DIR}{RECIPE_FILENAME}"
+
+RECIPE_PRICE_FILENAME = "RecetteWithPrice.txt"
+RECIPE_PRICE_FILE = f"{CURRENT_DIR}{RECIPE_PRICE_FILENAME}"
+
+# --------------------------------------------------------------------- [ MAIN ]
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
