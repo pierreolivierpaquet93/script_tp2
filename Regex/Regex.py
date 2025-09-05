3525677e58df860924f2f034237e1d95077552fa
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-09-05 15:32:11 -0400

    +validate_user_name() + main test

--- Regex/Regex.py
+++ Regex/Regex.py
@@ -1,6 +1,20 @@
+import re
 
-def main():
-	pass
+class RegularExpression:
+	@staticmethod
+	def validate_user_name( user_name ):
+		ERR_INVALID_NAME = "[Invalid Name]"
 
-if __name__ == "__main__":
-	main()
+		open_quote = "^[\"]?"
+		grp_first = "(?P<first>[a-zA-Z]+-?[a-zA-Z]*)"
+		sep = "[ .-;]"
+		grp_second = "(?P<second>[a-zA-Z]+)"
+		closing_quote = "[\"]?$"
+		pattern = fr"{open_quote}{grp_first}{sep}{grp_second}{closing_quote}"
+		matches = re.match( pattern, user_name )
+		
+		if matches:
+			print( f"Name First={matches.group("first")} - " +
+		 		f"Second={matches.group("second")}" )
+		else:
+			print( ERR_INVALID_NAME )
--- Regex/main.py
+++ Regex/main.py
@@ -1,1 +1,32 @@
-if
+import Regex
+
+def main():
+	user_names=[
+		'"Tremblay Jean"',
+		'Sophie LÃ©vesque',
+		'Dupont Marc',
+		'@Luc Parent',
+		'Lavoie;Catherine',
+		'Nadia GagnÃ©',
+		'Marc-AndrÃ©_Benoit',
+		'GeneviÃ¨ve.Dionne',
+		'"Boucher Alain"',
+		'Roxanne.Larose',
+		'"Dupuis Sophie"',
+		'HervÃ© Morin',
+		'FÃ©lix Tremblay',
+		'Julie Moreau',
+		'Alexandre Gagnon',
+		'Lalonde Claude',
+		'Couture Ã‰lise',
+		'Annie Therrien',
+		'GÃ©rard NoÃ«l',
+		'Marie-Claude.Nantel'
+	]
+
+	print("------Validation of User Names------")
+	for name in user_names:
+		Regex.RegularExpression.validate_user_name( name )
+
+if __name__ == "__main__":
+	main()
