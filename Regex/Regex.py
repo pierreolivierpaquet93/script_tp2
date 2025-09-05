52e78aca088bf8d32521ab0706e137881d8db110
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-09-05 18:07:36 -0400

    +validate_phone()

--- Regex/Regex.py
+++ Regex/Regex.py
@@ -2,8 +2,8 @@ import re
 
 class RegularExpression:
 	@staticmethod
-	def validate_user_name( user_name ):
-		ERR_INVALID_NAME = "[Invalid Name]"
+	def validate_user_name( user_name: str ) -> None:
+		err_invalid_name = "[Invalid Name]"
 
 		open_quote = "^[\"]?"
 		grp_first = "(?P<first>[a-zA-Z]+-?[a-zA-Z]*)"
@@ -17,4 +17,31 @@ class RegularExpression:
 			print( f"Name First={matches.group("first")} - " +
 		 		f"Second={matches.group("second")}" )
 		else:
-			print( ERR_INVALID_NAME )
+			print( err_invalid_name )
+
+	@staticmethod
+	def validate_phone( phone: str ) -> None:
+		err_invalid_phone = "[Invalid Phone Number]"
+		
+		open_paren = "\(?"
+		area_code = "\d{3}"
+		close_paren = "\)?"
+		grp_area = f"{open_paren}(?P<area>{area_code}){close_paren}"
+		prefix = "\d{3}"
+		grp_prefix = f"(?P<prefix>{prefix})"
+		line = "\d{4}"
+		grp_line = f"(?P<line>{line})"
+		sep = "[ .\-/]?"
+		pattern = (fr"^{grp_area}{sep}{grp_prefix}{sep}{grp_line}$")
+
+		matches = re.match( pattern, phone )
+		if matches:
+			print(
+				f"Phone Number Region={matches.group("area")} " +
+				f"Number={matches.group("prefix")}-{matches.group("line")}" )
+		else:
+			print( err_invalid_phone )
+
+	@staticmethod
+	def validate_birth_date( birth_date: str ) -> None:
+		pass
--- Regex/main.py
+++ Regex/main.py
@@ -1,6 +1,7 @@
 import Regex
 
 def main():
+	"""
 	user_names=[
 		'"Tremblay Jean"',
 		'Sophie LÃ©vesque',
@@ -27,6 +28,34 @@ def main():
 	print("------Validation of User Names------")
 	for name in user_names:
 		Regex.RegularExpression.validate_user_name( name )
+
+	phones = [
+		'5141234567',
+		'(514) 555-1234',
+		'514.555.7890',
+		'',
+		'438 123 4567',
+		'514-333-6789',
+		'1234567890',
+		'514 999 1234',
+		'(514)5551234',
+		'514/321/7890',
+		'514*123*1234',
+		'4385559999',
+		'514 222 1111',
+		'514.222.3333',
+		'(438)123-4567',
+		'514123456789',
+		'438-321-4567',
+		'514.444.0000',
+		'514123-7890',
+		'5141231234'
+	]
+	print("------Validation of Phones------")
+	for num in phones:
+		Regex.RegularExpression.validate_phone( num )
+	"""
+
 
 if __name__ == "__main__":
 	main()
