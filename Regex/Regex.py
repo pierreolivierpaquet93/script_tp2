d63f20a3bd9dc386b829d6c08d714d094c7323b9
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-09-05 21:57:58 -0400

    +validate_birth_date ; +validate_postal_code() ; +validate_user_id() ; + main tests

--- Regex/Regex.py
+++ Regex/Regex.py
@@ -1,16 +1,31 @@
 import re
 
+# ---------------------------------------------------------------------- [ DOC ]
+
+# https://stackoverflow.com/questions/1034573/most-idiomatic-way-to-convert-none-to-empty-string
+# https://www.canadapost-postescanada.ca/cpc/en/support/articles/addressing-guidelines/postal-codes.page
+
+# ------------------------------------------------------------------------------
+
+def nonestr( validate: str ) -> str:
+	if validate is None:
+		return ""
+	else:
+		return validate
+
 class RegularExpression:
 	@staticmethod
 	def validate_user_name( user_name: str ) -> None:
-		err_invalid_name = "[Invalid Name]"
+		err_invalid_name = '[Invalid Name]'
 
-		open_quote = "^[\"]?"
-		grp_first = "(?P<first>[a-zA-Z]+-?[a-zA-Z]*)"
-		sep = "[ .-;]"
-		grp_second = "(?P<second>[a-zA-Z]+)"
-		closing_quote = "[\"]?$"
-		pattern = fr"{open_quote}{grp_first}{sep}{grp_second}{closing_quote}"
+		open_quote =	r"^[\"]?"
+		grp_first =		r"(?P<first>[a-zA-Z]+-?[a-zA-Z]*)"
+		sep =			r"[ .-;]"
+		grp_second =	r"(?P<second>[a-zA-Z]+)"
+		closing_quote =	r"[\"]?$"
+		pattern =		(fr"{open_quote}{grp_first}{sep}" +
+			f"{grp_second}{closing_quote}")
+
 		matches = re.match( pattern, user_name )
 		
 		if matches:
@@ -21,18 +36,18 @@ class RegularExpression:
 
 	@staticmethod
 	def validate_phone( phone: str ) -> None:
-		err_invalid_phone = "[Invalid Phone Number]"
+		err_invalid_phone = '[Invalid Phone Number]'
 		
-		open_paren = "\(?"
-		area_code = "\d{3}"
-		close_paren = "\)?"
-		grp_area = f"{open_paren}(?P<area>{area_code}){close_paren}"
-		prefix = "\d{3}"
-		grp_prefix = f"(?P<prefix>{prefix})"
-		line = "\d{4}"
-		grp_line = f"(?P<line>{line})"
-		sep = "[ .\-/]?"
-		pattern = (fr"^{grp_area}{sep}{grp_prefix}{sep}{grp_line}$")
+		open_paren =	r"\(?"
+		area_code =		r"\d{3}"
+		close_paren =	r"\)?"
+		grp_area =		fr"{open_paren}(?P<area>{area_code}){close_paren}"
+		prefix =		r"\d{3}"
+		grp_prefix =	fr"(?P<prefix>{prefix})"
+		line =			r"\d{4}"
+		grp_line =		fr"(?P<line>{line})"
+		sep =			r"[ .\-/]?"
+		pattern =		fr"^{grp_area}{sep}{grp_prefix}{sep}{grp_line}$"
 
 		matches = re.match( pattern, phone )
 		if matches:
@@ -44,4 +59,86 @@ class RegularExpression:
 
 	@staticmethod
 	def validate_birth_date( birth_date: str ) -> None:
-		pass
+		err_invalid_birth_date = '[Invalid Birth Date]'
+		
+		# Format 1 -> "jj/mm/yyyy"
+		f1_sep =	r"/"
+		f1_day =	r"(?P<f1_day>\d{2})"
+		f1_month =	r"(?P<f1_month>\d{2})"
+		f1_year =	r"(?P<f1_year>\d{4})"
+		f1 =		fr"(^{f1_day}{f1_sep}{f1_month}{f1_sep}{f1_year}$)"
+
+		# Format 2 -> "yyyy-mm-jj"
+		f2_sep =	r"\-"
+		f2_year =	r"(?P<f2_year>\d{4})"
+		f2_month =	r"(?P<f2_month>\d{2})"
+		f2_day =	r"(?P<f2_day>\d{2})"
+		f2 =		fr"(^{f2_year}{f2_sep}{f2_month}{f2_sep}{f2_day}$)"
+
+		# Format 3 -> "yyyy.jj.mm"
+		f3_sep =	r"\."
+		f3_year =	r"(?P<f3_year>\d{4})"
+		f3_day =	r"(?P<f3_day>\d{2})"
+		f3_month =	r"(?P<f3_month>\d{2})"
+		f3 =		fr"(^{f3_year}{f3_sep}{f3_day}{f3_sep}{f3_month}$)"
+
+		pattern = fr"{f1}|{f2}|{f3}"
+
+		matches = re.match(pattern, birth_date)
+
+		if matches:
+			year = str(
+				matches.group( "f1_year" ) or
+				matches.group( "f2_year" ) or 
+				matches.group( "f3_year" )
+			)
+			month = str(
+				matches.group( "f1_month" ) or
+				matches.group( "f2_month" ) or
+				matches.group( "f3_month" )
+			)
+			day = str(
+				matches.group( "f1_day" ) or
+				matches.group( "f2_day" ) or
+				matches.group( "f3_day" )
+			)
+			print( f"Birth Date Year={year} Month={month} Day={day}" )
+		else:
+			print( err_invalid_birth_date )
+
+	@staticmethod
+	def validate_postal_code( postal_code: str ) -> None:
+		err_invalid_postal_code = '[Invalid Postal Code]'
+
+		fsa = r"(?P<fsa>[a-zA-Z][\d][a-zA-Z])"
+		sep = r"[ \-]?"
+		ldu = r"(?P<ldu>[\d][a-zA-Z][\d])"
+
+		pattern = fr"^{fsa}{sep}{ldu}$"
+
+		matches = re.match( pattern, postal_code )
+
+		if matches:
+			lhs = matches.group( "fsa" ).upper()
+			rhs = matches.group( "ldu" ).upper()
+			print( f"Postal Code = {lhs} {rhs}" )
+		else:
+			print( err_invalid_postal_code )
+
+
+	@staticmethod
+	def validate_user_id( user_id: str ) -> None:
+		err_invalid_user_id = '[Invalid User Id]'
+
+		prefix =	r"(?P<prefix>CLT\-)"
+		uid =		r"(?P<id>\d{3})"
+
+		pattern = fr"^{prefix}{uid}"
+
+		matches = re.match( pattern, user_id )
+
+		if matches:
+			user = matches.group( "id" )
+			print( f"User Id = {user}" )
+		else:
+			print( err_invalid_user_id )
--- Regex/main.py
+++ Regex/main.py
@@ -1,7 +1,6 @@
 import Regex
 
 def main():
-	"""
 	user_names=[
 		'"Tremblay Jean"',
 		'Sophie LÃ©vesque',
@@ -24,11 +23,11 @@ def main():
 		'GÃ©rard NoÃ«l',
 		'Marie-Claude.Nantel'
 	]
-
-	print("------Validation of User Names------")
+	print( "------Validation of User Names------" )
 	for name in user_names:
 		Regex.RegularExpression.validate_user_name( name )
-
+	print( "------------------------------------\n" )
+		
 	phones = [
 		'5141234567',
 		'(514) 555-1234',
@@ -51,11 +50,91 @@ def main():
 		'514123-7890',
 		'5141231234'
 	]
-	print("------Validation of Phones------")
+	print( "------Validation of Phones------" )
 	for num in phones:
 		Regex.RegularExpression.validate_phone( num )
-	"""
+	print( "------------------------------------\n" )
 
+	birth_dates = [
+		'01/07/1990',
+		'1992-12-05',
+		'05-13-1985',
+		'July 8th',
+		'1988/07/15',
+		'15 aoÃ»t 1991',
+		'07.07.1987',
+		'1986-06-06',
+		'06/06/1986',
+		'1984-01-01',
+		'1985/05/05',
+		'1983-11-30',
+		'1990-30-12',
+		'1993.12.12',
+		'1992/01/01',
+		'01-01-1991',
+		'02/14/1988',
+		'1980-02-29',
+		'07/07/77',
+		'31/02/1984'
+	]
+	print( "------Validation of Birth Dates------" )
+	for date in birth_dates:
+		Regex.RegularExpression.validate_birth_date( date )
+	print( "------------------------------------\n" )
 
+	postal_codes = [
+		'H2X 1Z1',
+		'H2Y1Z2',
+		'H3Z-9Y9',
+		' 1980',
+		'H3Z 2X2',
+		'H2Y1A1',
+		'H1Z-2K2',
+		'H2X1A1',
+		'H4X 1A3',
+		'h1x 2y2',
+		'H2X-2X2',
+		'H0H0H0',
+		'H3Z 1Z9',
+		'h2z1x1',
+		'H2W 1B2',
+		'H4A1A1',
+		'H2Y 2Z3',
+		'h1z 3x3',
+		'H3H 3H3',
+		'H2Z 2Z2'
+	]
+	print( "------Validation of Postal Codes------" )
+	for code in postal_codes:
+		Regex.RegularExpression.validate_postal_code( code )
+	print( "------------------------------------\n" )
+
+	user_ids = [
+		'CLT-001,',
+		'CLT-002,',
+		'CLT_003,',
+		'h3x1w4',
+		'CLT-005',
+		'CLT-006',
+		'CLT-007',
+		'CLT-008',
+		'CLT-009',
+		'CLT-010',
+		'CLT-011',
+		'CLT-012',
+		'CLT-013',
+		'CLT-014',
+		'CLT-015',
+		'CLT-016',
+		'CLT-017',
+		'CLT-018',
+		'CLT-019',
+		'CLT-020'
+	]
+	print( "------Validation of User Ids------" )
+	for id in user_ids:
+		Regex.RegularExpression.validate_user_id( id )
+	print( "------------------------------------\n" )
+		
 if __name__ == "__main__":
 	main()
