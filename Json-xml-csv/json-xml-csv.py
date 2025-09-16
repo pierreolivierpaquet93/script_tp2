a27091756e5e80d0b21041e2a5d812645b7c71cd
Pierre Olivier Paquet <pierreolivierpaquet93@gmail.com>
2025-09-16 09:56:46 -0400

    ADDED -> output json AND output xml ; TO DO -> output CSV + tests

--- Json-xml-csv/json-xml-csv.py
+++ Json-xml-csv/json-xml-csv.py
@@ -3,8 +3,20 @@ import json
 import csv
 import xml.etree.ElementTree as ET 
 
-JSON_FILENAME = "InputComputers.json"
-XML_FILENAME = "InputComputers.xml"
+# ----------------------------------------------------------------[ CONSTANT.S ]
+
+EXT_JSON = ".json"
+EXT_XML = ".xml"
+EXT_CSV = ".csv"
+
+INPUT_FILENAME = "InputComputers"
+INPUT_JSON = f"{INPUT_FILENAME}{EXT_JSON}"
+INPUT_XML = f"{INPUT_FILENAME}{EXT_XML}"
+
+OUTPUT_FILENAME = "OutputComputers"
+OUTPUT_JSON = f"{OUTPUT_FILENAME}{EXT_JSON}"
+OUTPUT_XML = f"{OUTPUT_FILENAME}{EXT_XML}"
+OUTPUT_CSV = f"{OUTPUT_FILENAME}{EXT_CSV}"
 
 BLU = "\033[1;34m"
 RST = "\033[0m"
@@ -13,6 +25,10 @@ FIELD_NAME = "Name"
 FIELD_MODEL = "Model"
 FIELD_CPU = "CPU"
 FIELD_RAM = "RAM_GB"
+
+# ------------------------------------------------------------------[ CLASSE.S ]
+
+# -------------------------------------------------------------[ Computer ]-----
 
 class Computer():
 	def __init__(
@@ -26,6 +42,20 @@ class Computer():
 		self._model: str = model
 		self._cpu: str = cpu
 		self._ram: int = ram
+
+	def GetName( self ) -> str:
+		return self._name
+	
+	def GetModel( self ) -> str:
+		return self._model
+	
+	def GetCpu( self ) -> str:
+		return self._cpu
+	
+	def GetRam( self ) -> int:
+		return self._ram
+ 
+# ---------------------------------------------------------------[ MyJson ]-----
 
 class MyJson():
 	def __init__(
@@ -43,6 +73,8 @@ class MyJson():
 	def GetParsed( self ):
 		return self._parsed
 
+# ----------------------------------------------------------------[ MyXml ]-----
+
 class MyXml():
 	def __init__(
 		self,
@@ -59,20 +91,25 @@ class MyXml():
 	def GetRoot( self ):
 		return self._root
 
+# ---------------------------------------------------------------[ Enonce ]-----
+
 class Enonce():
 	def __init__(
 		self,
 		my_json: MyJson = None,
 		my_xml: MyXml = None
 	):
+		self._path = os.path.dirname( __file__ ) + '\\'
 		self._computers: list[Computer] = []
 		self.AddFromJson( my_json )
 		self.AddFromXml( my_xml )
 	
 	def AddFromXml(
 		self,
-		my_xml: MyXml
+		my_xml: MyXml = None
 	):
+		if my_xml is None:
+			return
 		for new_computer in my_xml.GetRoot().findall( "Computer" ):
 			name = new_computer.find( FIELD_NAME ).text
 			model = new_computer.find( FIELD_MODEL ).text
@@ -88,8 +125,10 @@ class Enonce():
 
 	def AddFromJson(
 		self,
-		my_json: MyJson
+		my_json: MyJson = None
 	):
+		if my_json is None:
+			return
 		for new_computer in my_json.GetParsed():
 			name = new_computer[FIELD_NAME]
 			model = new_computer[FIELD_MODEL]
@@ -103,11 +142,54 @@ class Enonce():
 			)
 			self._computers.append( tmp )
 
+	def OutputJson( self ):
+		computer_lst: list[dict] = []
+		for computer in self._computers:
+			computer_dict = {
+				f"{FIELD_NAME}" : f"{computer.GetName()}",
+				f"{FIELD_MODEL}" : f"{computer.GetModel()}",
+				f"{FIELD_CPU}" : f"{computer.GetCpu()}",
+				f"{FIELD_RAM}" : int(computer.GetRam())
+			}
+			computer_lst.append( computer_dict )
+		with open( self._path + OUTPUT_JSON, 'w' ) as f:
+			computer_json_str = json.dump( computer_lst, f, indent=4 )
+
+	def OutputXml( self ):
+		root_computers = ET.Element( "Computers" )
+		for computer in self._computers:
+			sub_computer = ET.SubElement( root_computers, "Computer" )
+			sub_computer_name = ET.SubElement( sub_computer, FIELD_NAME )
+			sub_computer_name.text = computer.GetName()
+			sub_computer_model = ET.SubElement( sub_computer, FIELD_MODEL )
+			sub_computer_model.text = computer.GetModel()
+			sub_computer_cpu = ET.SubElement( sub_computer, FIELD_CPU )
+			sub_computer_cpu.text = computer.GetCpu()
+			sub_computer_ram = ET.SubElement( sub_computer, FIELD_RAM )
+			sub_computer_ram.text = str(computer.GetRam())
+			ET.indent( root_computers )
+			tree_computers = ET.ElementTree( root_computers )
+			tree_computers.write(
+				self._path + OUTPUT_XML,
+				encoding="utf-8",
+				xml_declaration=True
+			)
+
+	def OutputCsv( self ):
+		pass
+
+# ----------------------------------------------------------------------[ MAIN ]
+
 def main():
-	path = os.path.dirname(__file__)+'\\'
-	a = MyJson( path+JSON_FILENAME )
-	b = MyXml( path+XML_FILENAME )
-	Enonce(a, b)
+	path = os.path.dirname(__file__) + '\\'
+	input_json = MyJson( path + INPUT_JSON )
+	input_xml = MyXml( path + INPUT_XML )
+	travail_pratique = Enonce(
+		input_json,
+		input_xml
+	)
+	travail_pratique.OutputJson()
+	travail_pratique.OutputXml()
 
 if __name__ == "__main__":
 	main()
